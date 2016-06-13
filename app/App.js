02fcf9e5c02b56c9498c289b50e0fc2f1505fcf2
macksol <msolomon@whalerockindustries.com>
2016-06-13 07:23:27 -0700

    redirect to login if not authenticated

--- app/App.js
+++ app/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import styles from './App.css';
+import Firebase from 'firebase';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {test: 'foo'};
+  }
+  componentWillMount() {
+    var ref = new Firebase('https://emoji-dev.firebaseio.com/');
+    if (!ref.getAuth()) {
+      window.location.replace(window.location.origin);
+    }
   }
   render() {
     return (
