d6d7b0b702b6aeeb0b90653bde7968f341852686
macksol <msolomon@whalerockindustries.com>
2016-06-13 07:40:05 -0700

    asynchronously check user state and add .nvmrc file

--- app/App.js
+++ app/App.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import styles from './App.css';
 import Firebase from 'firebase';
+import ReactFireMixin from 'reactfire'
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {test: 'foo'};
-  }
-  componentWillMount() {
-    var ref = new Firebase('https://emoji-dev.firebaseio.com/');
-    if (!ref.getAuth()) {
-      window.location.replace(window.location.origin);
+
+
+var App = React.createClass({
+
+  mixins: [ReactFireMixin],
+
+  getDefaultProps() {
+    return {
+      test: 'foo'
     }
-  }
+  },
+
+  redirectToLogin() {
+    window.location.replace(window.location.origin);
+  },
+
+  monitorUserState(data) {
+    if (!data) {
+      this.redirectToLogin();
+    }
+  }, // asynchronously monitor user state
+
+  componentWillMount() {
+    console.log(this.state);
+    var ref = new Firebase('https://emoji-dev.firebaseio.com/');
+    console.log(ref);
+    if (ref.getAuth()) {
+      ref.onAuth(this.monitorUserState);
+    } else {
+      this.redirectToLogin();
+    }
+  },
+
   render() {
     return (
       <div className={styles.app}>
@@ -20,4 +43,6 @@ export default class App extends React.Component {
       </div>
     );
   }
-}
+});
+
+export default App
